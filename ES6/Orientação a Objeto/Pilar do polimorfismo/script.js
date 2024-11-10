2d32f4c560475b15dbdfbb692823b62520bac5f0
Pedro Lucas <82179817+PedroLucasGarcia@users.noreply.github.com>
2024-11-09 23:54:26 -0300

    Create Pilar do  polimorfismo

---
+++ ES6/Orientação a Objeto/Pilar do polimorfismo/script.js
@@ -0,0 +1,79 @@
+class Animal{  //Classe pai - propriedades em comum das classes filhas
+  constructor(cor, tamanho, peso){
+    this.cor = cor
+    this.tamanho = tamanho
+    this.peso = peso
+  }
+
+  dormir(){
+    console.log('dormir')
+  }
+}
+
+class Cachorro extends Animal{  //Classe filha - herda as propriedades da classe pai
+  constructor(){
+    super()  //Esse operdor precisa ser declarado em todas as classes filhas e deve ficar antes de qualquer atributo
+    this.orelha = 'Grandes e caídas'
+  }
+
+  correr(){
+    console.log('correr')
+  }
+
+  rosnar(){
+    console.log('rosnar')
+  }
+}
+
+class Passaro extends Animal{  //Classe filha - herda as propriedades da classe pai
+  constructor(bico, cor, tamanho, peso){
+    super(cor, tamanho, peso)  //Esse operdor precisa ser declarado em todas as classes filhas e deve ficar antes de qualquer atributo
+    this.bico = bico
+  }
+
+  voar(){
+    console.log('voar')
+  }
+}
+
+class Papaguaio extends Passaro{  //Classe filha - herda as propriedades da classe pai
+  constructor(sabeFalar, bico, cor, tamanho, peso){
+    super(bico, cor, tamanho, peso)  //Esse operdor precisa ser declarado em todas as classes filhas e deve ficar antes de qualquer atributo. Pode ter parâmetros ou não, caso tenha, será o valor dos atributos da classe pai
+    this.sabeFalar = sabeFalar
+  }
+
+  falar(){
+    console.log('falar')
+  }
+}
+
+class Avestruz extends Passaro{
+  constructor(bico, cor, tamanho, peso){
+    super(bico, cor, tamanho, peso)
+  }
+
+  voar(){  //Polimorfismo - consiste em sobrescrever um método herdado da classe pai e alterar de acordo com a nessessidade da classe filha
+    console.log('Não sabe voar')
+  }
+}
+
+let cachorro = new Cachorro()
+
+let passaro = new Passaro('Curto', 'azul', 35, 450)
+console.log(passaro)
+
+let papaguaio = new Papaguaio(true, 'médio', 'verde', 25, 350)
+console.log(papaguaio)
+
+let papaguaio2 = new Papaguaio(false, 'pequeno', 'branco', 10, 80)
+console.log(papaguaio2)
+
+let avestruz = new Avestruz('Médio', 'Preto e branco', 150, 1500)
+
+cachorro.dormir()
+passaro.dormir()
+papaguaio.falar()
+papaguaio.dormir()
+papaguaio.voar()
+avestruz.voar()
+
